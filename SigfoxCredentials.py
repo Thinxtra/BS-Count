07909066447932d314c4aaf1f7c9873a56c7de13
ThomasHoThinxtra <thomas.ho@thinxtra.com>
2018-04-23 17:36:42 +1000

    Add files via upload

---
+++ SigfoxCredentials.py
@@ -0,0 +1,12 @@
+class Credentials:
+
+    # Author : Adrian Tchordjallian <adrian.tchordjallian@thinxtra.com>
+
+    def __init__(self):
+        self.user = {'AU':'58058f79500574595d8d8c1c',
+                     'NZ': '5935d5863c87894cd50f6938',
+                     'HK': '597ac4e09e93a178a14a6035'}
+        self.password = {'AU':'3780af11d178d13247d4d149af9ea43e',
+                     'NZ': 'aa134f150dc1b3f4b16489c44c21b5eb',
+                     'HK': 'd27c32e5dabf8382b42f66d4bb3f7bba'}
+        self.country = 'Australia'
